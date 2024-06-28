0cb56043cf7abb2019b6a8b703c6d6e253732692
ginwan <ginwan697@gmail.com>
2024-06-28 14:12:56 +0400

    Sum the fibonacci array

--- math_algorithms/fibonacci_sequence.js
+++ math_algorithms/fibonacci_sequence.js
@@ -5,15 +5,18 @@
 
 var fibonacci = (n) => {
   const fib = [0, 1];
+  var sum = 1;
   for (let i = 2; i < n; i++) {
     fib[i] = fib[i - 1] + fib[i - 2];
+    // sum the fibonacci sequence array
+    sum += fib[i];
   }
-  return fib;
+  return { sequence: fib, sum: sum };
 };
 
 console.log("Fibonacci Sequence 1: ", fibonacci(2));
 console.log("Fibonacci Sequence 2: ", fibonacci(3));
 console.log("Fibonacci Sequence 3: ", fibonacci(7));
-console.log("Fibonacci Sequence 4: ", fibonacci(5));
+// console.log("Fibonacci Sequence 4: ", fibonacci(5));
 
 // Big-O = O(n)
