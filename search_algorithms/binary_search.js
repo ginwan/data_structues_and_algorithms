4ada86fe6f470940ff271b240553c2de3973ccb0
ginwan <ginwan697@gmail.com>
2024-07-02 10:03:53 +0400

    Binary search

---
+++ search_algorithms/binary_search.js
@@ -0,0 +1,28 @@
+/** Binary search */
+
+//** Problem: Given a sorted array of "n" elements and a target element "t", find the index of "t" in the array. Return -1 if the target element is not found. */
+
+const binarySearch = (arr, target) => {
+  // Determine left and right index
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+
+  while (leftIndex <= rightIndex) {
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (target === arr[middleIndex]) {
+      return middleIndex;
+    }
+    if (target < arr[middleIndex]) {
+      rightIndex = middleIndex - 1;
+    } else {
+      leftIndex = middleIndex + 1;
+    }
+  }
+  return -1;
+};
+
+console.log(binarySearch([-5, 2, 4, 6, 10], 10)); // 4
+console.log(binarySearch([-5, 2, 4, 6, 10], 6)); // 3
+console.log(binarySearch([-5, 2, 4, 6, 10], 20)); // -1
+
+//** Big-O = O(log n) */
